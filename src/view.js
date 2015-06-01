34375f712413d7a3a7b2a511cc93e2f0b2e898d9
Alex Nichol <unixpickle@gmail.com>
2015-06-01 12:10:52 -0400

    draw using canvas; slower on firefox

--- src/view.js
+++ src/view.js
@@ -1,38 +1,69 @@
 var ELEMENT_ID = 'pentagon-background'
 
 function PentagonView() {
+  this._canvas = document.createElement('canvas');
   this._element = document.getElementById(ELEMENT_ID);
   if (!this._element) {
     this._element = document.createElement('div');
     this._element.id = ELEMENT_ID;
-  }
-
-  this._pentagonElements = [];
-  for (var i = 0, len = Pentagon.allPentagons.length; i < len; ++i) {
-    var element = document.createElement('img');
-    element.style.display = 'block';
-    element.style.position = 'fixed';
-    element.style.left = 0;
-    element.style.top = 0;
-    element.style.opacity = 0;
-    this._pentagonElements[i] = element;
-    this._element.appendChild(element);
-  }
-  this._imageSize = 0;
-  this._width = window.innerWidth;
-  this._height = window.innerHeight;
-  this._imageCache = {};
-  this._updateImage();
-
-  if (!this._element.parentNode) {
     document.body.insertBefore(this._element, document.body.childNodes[0] ||
       null);
   }
+
+  makeAbsoluteAndFullScreen(this._element);
+  makeAbsoluteAndFullScreen(this._canvas);
+
+  this._element.appendChild(this._canvas);
+
+  this._imageSize = 0;
+  this._imageCache = {};
+  this._width = 0;
+  this._height = 0;
+  this._updateSize();
+  this._updateImage();
+
   window.addEventListener('resize', this._updateAll.bind(this));
-  this._requestAnimationFrame();
+  this._tick();
 }
 
-PentagonView.prototype._computeImageData = function() {
+PentagonView.prototype._drawPentagons = function() {
+  var image = this._pentagonImage();
+  var context = this._canvas.getContext('2d');
+  
+  context.clearRect(0, 0, this._width, this._height);
+  
+  var size = Math.max(this._width, this._height);
+  var xOffset = 0;
+  var yOffset = 0;
+  if (this._width < this._height) {
+    xOffset = -(this._height - this._width) / 2;
+  } else {
+    yOffset = -(this._width - this._height) / 2;
+  }
+  
+  for (var i = 0, len = Pentagon.allPentagons.length; i < len; ++i) {
+    var frame = Pentagon.allPentagons[i].frame();
+
+    var translateX = frame.x*size + xOffset;
+    var translateY = frame.y*size + yOffset;
+    var scale = 2 * size * frame.radius / this._imageSize;
+
+    context.globalAlpha = frame.opacity;
+    context.translate(translateX, translateY);
+    context.rotate(frame.rotation);
+    context.scale(scale, scale);
+    context.translate(-this._imageSize/2, -this._imageSize/2);1
+    context.drawImage(image, 0, 0);
+    // NOTE: save()/reset() are apparentlty slow, although this is mainly a
+    // premature optimization.
+    context.translate(this._imageSize/2, this._imageSize/2);1
+    context.scale(1/scale, 1/scale);
+    context.rotate(-frame.rotation);
+    context.translate(-translateX, -translateY);
+  }
+};
+
+PentagonView.prototype._pentagonImage = function() {
   var imageSize = this._imageSize;
 
   if (this._imageCache.hasOwnProperty('' + imageSize)) {
@@ -58,38 +89,10 @@ PentagonView.prototype._computeImageData = function() {
   context.closePath();
   context.fill();
 
-  var imageData = canvas.toDataURL('image/png');
-  this._imageCache['' + imageSize] = imageData;
-  return imageData;
-};
-
-PentagonView.prototype._layoutPentagons = function() {
-  var xOffset = 0;
-  var yOffset = 0;
-  var size = Math.max(this._width, this._height);
-  if (this._width < this._height) {
-    xOffset = -(this._height - this._width) / 2;
-  } else {
-    yOffset = -(this._width - this._height) / 2;
-  }
-  for (var i = 0, len = this._pentagonElements.length; i < len; ++i) {
-    var frame = Pentagon.allPentagons[i].frame();
-    var element = this._pentagonElements[i];
-
-    var translateX = frame.x*size - this._imageSize/2 + xOffset;
-    var translateY = frame.y*size - this._imageSize/2 + yOffset;
-    var angle = frame.rotation * 180 / Math.PI;
-    var scale = 2 * size * frame.radius / this._imageSize;
-
-    var transform = 'translate(' + translateX.toPrecision(5) + 'px, ' +
-      translateY.toPrecision(5) + 'px) rotate(' + angle.toPrecision(5) +
-      'deg) scale(' + scale.toPrecision(5) + ', ' + scale.toPrecision(5) + ')';
-    element.style.opacity = frame.opacity;
-    element.style.transform = transform;
-    element.style.webkitTransform = transform;
-    element.style.MozTransform = transform;
-    element.style.msTransform = transform;
-  }
+  var image = document.createElement('img');
+  image.src = canvas.toDataURL('image/png');
+  this._imageCache['' + imageSize] = image;
+  return image;
 };
 
 PentagonView.prototype._requestAnimationFrame = function() {
@@ -97,14 +100,14 @@ PentagonView.prototype._requestAnimationFrame = function() {
 };
 
 PentagonView.prototype._tick = function() {
-  this._layoutPentagons();
+  this._drawPentagons();
   this._requestAnimationFrame();
 };
 
 PentagonView.prototype._updateAll = function() {
   this._updateSize();
   this._updateImage();
-  this._layoutPentagons();
+  this._drawPentagons();
 };
 
 PentagonView.prototype._updateImage = function() {
@@ -115,14 +118,19 @@ PentagonView.prototype._updateImage = function() {
     return;
   }
   this._imageSize = imageSize;
-
-  var imageData = this._computeImageData();
-  for (var i = 0, len = this._pentagonElements.length; i < len; ++i) {
-    this._pentagonElements[i].src = imageData;
-  }
 };
 
 PentagonView.prototype._updateSize = function() {
   this._width = window.innerWidth;
   this._height = window.innerHeight;
+  this._canvas.width = this._width;
+  this._canvas.height = this._height;
 };
+
+function makeAbsoluteAndFullScreen(element) {
+  element.style.position = 'fixed';
+  element.style.top = 0;
+  element.style.left = 0;
+  element.style.width = '100%';
+  element.style.height = '100%';
+}
